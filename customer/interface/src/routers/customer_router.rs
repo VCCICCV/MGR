69ac23be353190bb457365ac8d5bde220ef3a39e
vcciccv <chaochen497@gmail.com>
2024-11-30 09:22:53 +0800

    feat: add query

--- customer/interface/src/routers/customer_router.rs
+++ customer/interface/src/routers/customer_router.rs
@@ -3,10 +3,8 @@ use axum::routing::post;
 use crate::{ api::customer_handler::{ active, sign_up, sign_in, sign_in_2fa }, state::AppState };
 pub async fn setup_customer_routers(router: Router<AppState>) -> Router<AppState> {
     router
-        // .route("/api/login", post(login))
         .route("/api/sign_up", post(sign_up))
         .route("/api/active", post(active))
         .route("/api/sign_in", post(sign_in))
         .route("/api/sign_in_2fa", post(sign_in_2fa))
-    // .route("/:id", delete(delete_user))
 }
